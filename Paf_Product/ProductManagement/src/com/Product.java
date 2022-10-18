8b553975aecad323b8d8a3806119715643873e25
Ramesh Lakmal <66841859+rameshlakmal@users.noreply.github.com>
2022-10-18 20:28:15 +0530

    Add files via upload

---
+++ Paf_Product/ProductManagement/src/com/Product.java
@@ -0,0 +1,173 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+public class Product {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/product?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertProject(String pName, String desct, String pPrice) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into productservice(`pId`, `pName`, `desct`, `pPrice`)"
+					+ " values ( ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, pName);
+			preparedStmt.setString(3, desct);
+			preparedStmt.setString(4, pPrice);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\": \"Error while inserting the project.\"}";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String readProject() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\'1\'><tr><th>Product Name</th><th>Description</th><th>Product Price</th><th>Update</th><th>Delete</th></tr>";
+			String query = "select * from productservice";
+
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String pID = Integer.toString(rs.getInt("pID"));
+				String pName = rs.getString("pName");
+				String desct = rs.getString("desct");
+				String pPrice = rs.getString("pPrice");
+
+				// Add into the html table
+				output += "<tr><td><input id=\'hidProjectIDUpdate\' name=\'hidProjectIDUpdate\' type=\'hidden\' value=\'"
+						+ pID + "'>" + pName + "</td>";
+				output += "<td>" + desct + "</td>";
+				output += "<td>" + pPrice + "</td>";
+
+				
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-productid='"
+						+ pID + "'>" + "</td></tr>";
+			}
+
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the projects.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String updateProject(String pID, String pName, String desct, String pPrice) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+
+			// create a prepared statement
+			String query = "UPDATE productservice SET pName=?,desct=?,pPrice=? WHERE pID=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+
+			preparedStmt.setString(1, pName);
+			preparedStmt.setString(2, desct);
+			preparedStmt.setString(3, pPrice);
+			preparedStmt.setInt(4, Integer.parseInt(pID));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\": \"Error while updating the project.\"}";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String deleteProject(String pID) {
+
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+
+			// create a prepared statement
+			String query = "delete from productservice where pID=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(pID));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "Error while deleting the project.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+}
---
+++ Paf_Product/product.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2021 at 07:18 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productservice`
+--
+
+CREATE TABLE IF NOT EXISTS `productservice` (
+  `pID` int(6) NOT NULL AUTO_INCREMENT,
+  `pName` varchar(60) NOT NULL,
+  `desct` varchar(255) NOT NULL,
+  `pPrice` varchar(10) NOT NULL,
+  PRIMARY KEY (`pID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
